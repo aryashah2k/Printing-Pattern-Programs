01377c712402d80eff2489ba073dc0baf98db850
Arya Shah <arya.shah82@nmims.edu.in>
2020-12-21 18:13:37 +0530

    Add Py Wave Pattern 9 Code

---
+++ Python Pattern Programs/Wave Patterns/Pattern 9.py
@@ -0,0 +1,19 @@
+wH = 4  # 1
+wL = 4  # 2
+pc = wH - 1  # 3
+
+for x in range(0, wH + 1):
+    for y in range(0, (wH * wL * 2)):
+        if (y % (wH * 2) == pc or
+                y % (wH * 2) == wH + x):
+            print("|", end="")
+        else:
+            print(" ", end="")  # single space
+    pc -= 1
+    print()
+
+    """
+1) change value to increase/decrease the height of the wave
+2) change value to increase/decrease the length of the wave
+3) cond for printing
+    """
