8420935bba190431d7e0bdb52cea75acf771a108
Arya Shah <arya.shah82@nmims.edu.in>
2020-12-23 09:30:08 +0530

    Add Py Wave Pattern 26

---
+++ Python Pattern Programs/Wave Patterns/Pattern 26.py
@@ -0,0 +1,17 @@
+wH = 5  # 1
+wL = 4  # 2
+
+for x in range(1, wH + 1):
+    for y in range(1, wL + 1):
+        for z in range(1, wH + 1):
+            if (x == z or x + z == wH + 1):
+                print("][", end="")
+            else:
+                print("  ", end="")  # 3
+    print()
+
+    """
+1) wH - change value to increase/decrease the height of the wave
+2) wL - change value to increase/decrease the length of the wave
+3) 2 whitespaces
+    """
