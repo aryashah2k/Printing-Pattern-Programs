049073de56ecffe80fb1d0a52357b08d5e50866f
Arya Shah <arya.shah82@nmims.edu.in>
2021-01-16 08:32:43 +0530

    Add Symbol Pattern 17 Code Py

---
+++ Python Pattern Programs/Symbol Patterns/Pattern 17.py
@@ -0,0 +1,12 @@
+n = 5  # size=5
+px = n
+py = n
+for x in range(1, n + 1):
+    for y in range(1, n * 2):
+        if (y <= x or y >= n * 2 - x):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    px -= 1
+    py += 1
+    print()
