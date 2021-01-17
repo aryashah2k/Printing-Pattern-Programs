43c4f6a7ec46145fce9cfa427cab9f95e2563194
Arya Shah <arya.shah82@nmims.edu.in>
2021-01-17 10:31:26 +0530

    Add Symbol Pattern 37 Code Py

---
+++ Python Pattern Programs/Symbol Patterns/Pattern 37.py
@@ -0,0 +1,8 @@
+n = 3
+for x in range(n, 0, -1):
+    for s1 in range(n - 1, 0, -1):
+        for s2 in range(n - 1, x - 1, -1):
+            print(" ", end=" ")
+        for y in range(0, x):
+            print("*", end=" ")
+        print()
