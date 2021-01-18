d664f08ab854b4c1931271c29cb41940107c0873
Arya Shah <arya.shah82@nmims.edu.in>
2021-01-18 09:23:33 +0530

    Add Symbol Pattern 69 Code Py

---
+++ Python Pattern Programs/Symbol Patterns/Pattern 69.py
@@ -0,0 +1,12 @@
+n = 4
+for x in range(n, -1, -1):
+    for y in range(n, -1, -1):
+        if y > x:
+            print("*", end=" ")
+        else:
+            print(chr(y+65), end=" ")
+    print()
+
+    """
+65 > ASCII of 'A'
+    """
