5e176eada08723a512195070c14e2ada82b3168c
Arya Shah <arya.shah82@nmims.edu.in>
2021-01-20 09:45:41 +0530

    Add Alphabet Pattern 46 Code Py

---
+++ Python Pattern Programs/Alphabetic Patterns/Pattern 46.py
@@ -0,0 +1,8 @@
+n = 5  # size
+for x in range(1, n + 1):
+    for y in range(1, x + 1):
+        if (x % 2 != 0):
+            print(chr(x + 64), end="")  # for char
+        else:
+            print(x, end="")
+    print()
