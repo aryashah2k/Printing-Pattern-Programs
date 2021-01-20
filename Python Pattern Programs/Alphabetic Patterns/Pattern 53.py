252f3de2546307bf835e286f39c30cb171612fb7
Arya Shah <arya.shah82@nmims.edu.in>
2021-01-20 09:47:55 +0530

    Add Alphabet Pattern 53 Code Py

---
+++ Python Pattern Programs/Alphabetic Patterns/Pattern 53.py
@@ -0,0 +1,8 @@
+n = 5  # size
+for x in range(n, 0, -1):
+    for y in range(1, x + 1):
+        if (x % 2 == 0):
+            print(chr(y + 64), end="")  # for char
+        else:
+            print(y, end="")
+    print()
