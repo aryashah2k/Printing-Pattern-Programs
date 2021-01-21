b604f09f3df994d271d89242118b6eedf0cc9a4f
Arya Shah <arya.shah82@nmims.edu.in>
2021-01-21 08:20:39 +0530

    Add Number Pattern 26 Code Py

---
+++ Python Pattern Programs/Numeric Patterns/Pattern 27.py
@@ -0,0 +1,7 @@
+size = 3
+for x in range(size, -(size + 1), -1):
+    for y in range(abs(x), 0, -1):
+        print(" ", end=" ")
+    for z in range(abs(x), 4):
+        print(z, end=" ")
+    print()
