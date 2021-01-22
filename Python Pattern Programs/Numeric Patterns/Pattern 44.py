a42a39ce60758ff8af9fcc0bed49d11025aae763
Arya Shah <arya.shah82@nmims.edu.in>
2021-01-22 16:00:46 +0530

    Add Numeric Pattern 44 Code Py

---
+++ Python Pattern Programs/Numeric Patterns/Pattern 44.py
@@ -0,0 +1,8 @@
+n = 5  # size
+for x in range(n, 0, -1):
+    for y in range(n, 0, -1):
+        if (x == y or x + y == n + 1):
+            print(y, end="")
+        else:
+            print(" ", end="")
+    print()
