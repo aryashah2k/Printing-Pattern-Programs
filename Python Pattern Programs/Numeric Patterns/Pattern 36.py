08666e2d92df49a5e0cf747ecccb281f7e020d08
Arya Shah <arya.shah82@nmims.edu.in>
2021-01-22 15:58:21 +0530

    Add Numeric Pattern 36 Code Py

---
+++ Python Pattern Programs/Numeric Patterns/Pattern 36.py
@@ -0,0 +1,12 @@
+n = 5  # size=5
+px = n
+py = n
+for x in range(1, n + 1):
+    for y in range(1, n * 2 + 1):
+        if (y == px or y == py):
+            print(px, end="")
+        else:
+            print(" ", end="")
+    px -= 1
+    py += 1
+    print()
