94bf24a20997e11a7f787949ed42350f74af4d14
Arya Shah <arya.shah82@nmims.edu.in>
2021-02-15 14:44:48 +0530

    Merge Alternative Solution | Python Pattern 5

--- Python Pattern Programs/Symbol Patterns/Pattern 5.py
+++ Python Pattern Programs/Symbol Patterns/Pattern 5.py
@@ -4,3 +4,13 @@ for x in range(5, 0, -1):
     for z in range(0, x):
         print("*", end="")
     print()
+
+# Another Logic
+
+for x in range(5, 0, -1):
+    for y in range(5, 0, -1):
+        if x >= y:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
