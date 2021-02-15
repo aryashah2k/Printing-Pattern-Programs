7107f0845e0de2f2e49377bef6eadb5e3ee46b7f
Arya Shah <arya.shah82@nmims.edu.in>
2021-02-15 14:24:48 +0530

    Merge Alternative Solution | C++ Program 34

--- CPlusPlus Pattern Programs/Symbol Patterns/Program 34.cpp
+++ CPlusPlus Pattern Programs/Symbol Patterns/Program 34.cpp
@@ -25,3 +25,34 @@ int main()
             cout<<endl;
     }
 }
+
+//Alternative Logic Using If-Else
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter Number Of Rows:"<<endl;
+    cin>>n;
+
+    int px=n;//Left Print Control
+    int py=n;//Right Print Control
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<n*2;j++)
+        {
+            if(j>=px && j<=py)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        px--;
+        py++;
+        cout<<endl;
+    }
+}
