333d88c239175e045ea584786b851c5da44fed40
Arya Shah <arya.shah82@nmims.edu.in>
2021-02-15 14:49:16 +0530

    Merge Alternative Solution | Python Pattern 4

--- Python Pattern Programs/Alphabetic Patterns/Pattern 4 Alternative.py
+++
@@ -1,5 +0,0 @@
-#Alternative
-for x in 'EDCBA':
-    for y in 'EDCBA':
-        print(y,end="")
-    print()
--- Python Pattern Programs/Alphabetic Patterns/Pattern 4.py
+++ Python Pattern Programs/Alphabetic Patterns/Pattern 4.py
@@ -2,3 +2,9 @@ for x in range(1, 6):
     for y in range(69, 64, -1):
         print(chr(y), end="")
     print()
+
+#Alternative
+for x in 'EDCBA':
+    for y in 'EDCBA':
+        print(y,end="")
+    print()
