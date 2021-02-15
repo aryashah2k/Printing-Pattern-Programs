3c0237951d8568a3ae301d8f8c34ff3fa473d42d
Arya Shah <arya.shah82@nmims.edu.in>
2021-02-15 14:37:27 +0530

    Merge Alternative Solution | Java Program 11

--- Java Pattern Programs/Symbol Patterns/Pattern 11.java
+++ Java Pattern Programs/Symbol Patterns/Pattern 11.java
@@ -20,3 +20,30 @@ class Pattern_69
     }
 }
 
+/-- Another Logic using if-else --/
+class Pattern_69
+{
+
+    public static void main(String args[])
+    {
+        int n = 5; //size
+
+        for (int i = n; i >= 1; i--)
+        {
+            for (int j = n; j >= 1; j--)
+            {
+                if (i >= j)
+                {
+                    System.out.print("* ");  // space after '*'
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
+/--------------------------------/
