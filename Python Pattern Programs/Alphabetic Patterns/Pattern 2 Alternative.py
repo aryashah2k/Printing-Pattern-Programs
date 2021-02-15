2f5eda9f3155496b481ffabc0ba102c1265ce50d
Arya Shah <arya.shah82@nmims.edu.in>
2021-02-15 14:47:55 +0530

    Merge Alternative Solution | Python Pattern 2

--- Python Pattern Programs/Alphabetic Patterns/Pattern 2 Alternative.py
+++
@@ -1,5 +0,0 @@
-#Alternative
-for x in 'ABCDE':
-    for y in 'ABCDE':
-        print(y,end="")
-    print()
--- Python Pattern Programs/Alphabetic Patterns/Pattern 2.py
+++ Python Pattern Programs/Alphabetic Patterns/Pattern 2.py
@@ -2,3 +2,9 @@ for x in range(1, 6):
     for y in range(65, 70):
         print(chr(y), end="")
     print()
+
+#Alternative
+for x in 'ABCDE':
+    for y in 'ABCDE':
+        print(y,end="")
+    print()
