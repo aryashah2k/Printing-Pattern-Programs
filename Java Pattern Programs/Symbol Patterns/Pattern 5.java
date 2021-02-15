6a95a6a0825afab56d7dc61104f556b86e83787d
Arya Shah <arya.shah82@nmims.edu.in>
2021-02-15 14:35:17 +0530

    Merge Alternative Solution | Java Program 5

--- Java Pattern Programs/Symbol Patterns/Pattern 5.java
+++ Java Pattern Programs/Symbol Patterns/Pattern 5.java
@@ -19,3 +19,31 @@ class Pattern_29
     
    }
 }
+
+   /-- Another Logic using if-else --/
+class Pattern_29
+{
+
+   public static void main(String args[])
+   {
+    int n = 5; //size
+   
+    for (int i = n;i >= 1;i--)
+    {
+     for (int j = n;j >= 1;j--)
+     {
+      if (i >= j)
+      {
+      System.out.print("*");
+      }
+      else
+      {
+      System.out.print(" ");
+      }
+     }
+     System.out.println();
+    }
+    
+   }
+}
+/--------------------------------/
