dc051126feac487cc12de49fefed24f2638d8e0d
Arya Shah <arya.shah82@nmims.edu.in>
2021-02-15 14:50:13 +0530

    Merge Alternative Solution | Python Pattern 79

--- Python Pattern Programs/Alphabetic Patterns/Pattern 79.py
+++ Python Pattern Programs/Alphabetic Patterns/Pattern 79.py
@@ -5,3 +5,14 @@ for x in 'EDCBA':
    else:
       print(x, end=" ")
  print()
+
+
+#Another way
+
+for x in range(69, 64, -1):
+    for y in range(65, 70):
+      if y < x:
+       print(chr(y), end=" ")
+      else:
+       print(chr(x), end=" ")
+    print()
