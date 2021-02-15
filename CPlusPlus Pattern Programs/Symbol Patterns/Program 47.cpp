b3b5c3bd4a018d15d7a3be56af58ee75555fe751
Arya Shah <arya.shah82@nmims.edu.in>
2021-02-15 14:24:13 +0530

    Merge Alternative Solution | C++ Program 47

--- CPlusPlus Pattern Programs/Symbol Patterns/Program 47.cpp
+++ CPlusPlus Pattern Programs/Symbol Patterns/Program 47.cpp
@@ -22,3 +22,33 @@ int main()
    }
 }
 
+// Alternative Way:
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout<<"Enter Number Of Rows:"<<endl;
+  cin>>n;
+  int px=1;
+  int py=n*2-1;
+  for(int i=1;i<=n;i++)
+  {
+      for(int j=1;j<n*2;j++)
+      {
+          if(j>=px && j<=py)
+          {
+              cout<<"*";
+          }
+          else
+          {
+              cout<<" ";
+          }
+      }
+      px++;
+      py--;
+      cout<<endl;
+  }
+}
+
