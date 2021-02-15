c01dc77f3811e25d03d1e05db82d09f57d893806
Arya Shah <arya.shah82@nmims.edu.in>
2021-02-15 14:34:47 +0530

    Merge Alternative Solution | Java Program 4

--- Java Pattern Programs/Symbol Patterns/Pattern 4.java
+++ Java Pattern Programs/Symbol Patterns/Pattern 4.java
@@ -22,3 +22,29 @@ class Pattern_24
    }
    
 
+----Alternative Solution----
+
+class Pattern_24
+{
+
+   public static void main(String args[])
+   {
+    int n = 5; //size
+    
+    for (int i = 1;i <= n;i++)
+    {
+     for (int j = n;j >= 1;j--)
+     {
+      if (i >= j)
+      {
+      System.out.print("*");
+      }
+      else
+      {
+      System.out.print(" ");
+      }
+     }
+     System.out.println();
+    }
+   
+   }
