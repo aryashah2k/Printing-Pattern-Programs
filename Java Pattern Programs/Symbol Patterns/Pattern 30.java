d3797f6357346cf692b05bb789b0215452f4c2bd
Arya Shah <arya.shah82@nmims.edu.in>
2021-02-15 14:39:09 +0530

    Merge Alternative Solution | Java Program 30

--- Java Pattern Programs/Symbol Patterns/Pattern 30.java
+++ Java Pattern Programs/Symbol Patterns/Pattern 30.java
@@ -17,4 +17,24 @@
  }
  } */
  
+ //logic two using if-else
+ for(i=1;i<=6;i++)
+ {
+ /*if(i%2!=0)
+ {
+ k=i+1;
+ }else
+ {
+ k=i;
+ }*/
  
+ k=(i%2!=0)?i+1:i;  //conditional operator
+ 
+ for(j=0;j<k;j++)
+ {
+ System.out.print("*");
+ }
+ System.out.println();
+ }
+     } //end of main
+ }  //end of class
