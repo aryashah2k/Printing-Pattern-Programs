bf6d66182938fea064825fadc177985617705221
Arya Shah <arya.shah82@nmims.edu.in>
2021-02-15 14:49:50 +0530

    Merge Alternative Solution | Python Pattern 78

--- Python Pattern Programs/Alphabetic Patterns/Pattern 78.py
+++ Python Pattern Programs/Alphabetic Patterns/Pattern 78.py
@@ -5,3 +5,14 @@ for x in 'ABCDE':
    else:
       print(x, end=" ")
  print()
+
+
+#Another way
+
+for x in range(65, 70):
+    for y in range(65, 70):
+      if y < x:
+       print(chr(y), end=" ")
+      else:
+       print(chr(x), end=" ")
+    print()
