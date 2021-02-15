efc3cd85be740101d265fb5c478d27bf0dd4a380
Arya Shah <arya.shah82@nmims.edu.in>
2021-02-15 14:44:14 +0530

    Merge Alternative Solution | Python Pattern 4

--- Python Pattern Programs/Symbol Patterns/Pattern 4 Alernative.py
+++
@@ -1,8 +0,0 @@
-# Another Logic
-for x in range(1, 6):
-    for y in range(5, 0, -1):
-        if x >= y:
-            print("*", end="")
-        else:
-            print(" ", end="")
-    print()
--- Python Pattern Programs/Symbol Patterns/Pattern 4.py
+++ Python Pattern Programs/Symbol Patterns/Pattern 4.py
@@ -4,3 +4,12 @@ for x in range(1, 6):
     for z in range(0, x):
         print("*", end="")
     print()
+
+# Another Logic
+for x in range(1, 6):
+    for y in range(5, 0, -1):
+        if x >= y:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
