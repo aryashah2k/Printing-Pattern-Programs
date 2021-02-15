bc88ae6b01e11d7e80152a2d35329c4e6aa58850
Arya Shah <arya.shah82@nmims.edu.in>
2021-02-15 14:25:29 +0530

    Merge Alternative Solution | C++ Program 29

--- CPlusPlus Pattern Programs/Symbol Patterns/Program 29.cpp
+++ CPlusPlus Pattern Programs/Symbol Patterns/Program 29.cpp
@@ -18,3 +18,28 @@ int main()
             cout<<endl;
     }
 }
+
+//Alternative Way Using If-Else Logic
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter Number Of Rows:"<<endl;
+    cin>>n;
+    for(int i=n;i>=1;i--)
+    {
+        for(int j=n;j>=1;j--)
+        {
+            if(i>=j)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
